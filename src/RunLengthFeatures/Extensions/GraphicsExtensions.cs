20d63bb4322381543d086f2de5d9aed08c0e092f
Konrad <konrjas@gmail.com>
2017-01-28 16:24:32 +0100

    ToGrayscaleMat extension

--- src/RunLengthFeatures/Extensions/GraphicsExtensions.cs
+++ src/RunLengthFeatures/Extensions/GraphicsExtensions.cs
@@ -7,23 +7,28 @@ namespace RunLengthFeatures.Extensions
 {
 	public static class GraphicsExtensions
 	{
-		public static BitmapImage ToGrayscale(this BitmapImage bmp)
+		public static Mat ToGrayscaleMat(this BitmapSource bmp)
 		{
 			var encoder = new JpegBitmapEncoder();
 			encoder.Frames.Add(BitmapFrame.Create(bmp));
 			using (var ms = new MemoryStream())
 			{
 				encoder.Save(ms);
-				using (var grayscaleMat = Mat.FromStream(ms, ImreadModes.GrayScale))
-				{
-					PerformThresholding(grayscaleMat);
-					var grayscaleImgStream = grayscaleMat.ToMemoryStream();
-					var result = new BitmapImage();
-					result.BeginInit();
-					result.StreamSource = grayscaleImgStream;
-					result.EndInit();
-					return result;
-				}
+				return Mat.FromStream(ms, ImreadModes.GrayScale);
+			}
+		}
+
+		public static BitmapImage ToGrayscale(this BitmapImage bmp)
+		{
+			using (var grayscaleMat = bmp.ToGrayscaleMat())
+			{
+				PerformThresholding(grayscaleMat);
+				var grayscaleImgStream = grayscaleMat.ToMemoryStream();
+				var result = new BitmapImage();
+				result.BeginInit();
+				result.StreamSource = grayscaleImgStream;
+				result.EndInit();
+				return result;
 			}
 		}
 
