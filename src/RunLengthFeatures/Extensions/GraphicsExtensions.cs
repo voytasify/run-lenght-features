a9a238395bc8558f9aa0b308a9b15ca7d0714bce
Konrad <konrjas@gmail.com>
2017-01-28 15:07:04 +0100

    OpenCVSharp + basic grayscale conversion

--- src/RunLengthFeatures/Extensions/GraphicsExtensions.cs
+++ src/RunLengthFeatures/Extensions/GraphicsExtensions.cs
@@ -1,26 +1,26 @@
-﻿using System.Windows.Media;
+﻿using System.IO;
 using System.Windows.Media.Imaging;
+using OpenCvSharp;
 
 namespace RunLengthFeatures.Extensions
 {
 	public static class GraphicsExtensions
 	{
-		public static FormatConvertedBitmap ToGrayscale(this BitmapImage bmp)
+		public static BitmapImage ToGrayscale(this BitmapImage bmp)
 		{
-			var grayscaleBmp = new BitmapImage();
-
-			grayscaleBmp.BeginInit();
-			grayscaleBmp.UriSource = bmp.UriSource;
-			grayscaleBmp.EndInit();
-
-			var grayscaleFormatBmp = new FormatConvertedBitmap();
-
-			grayscaleFormatBmp.BeginInit();
-			grayscaleFormatBmp.Source = grayscaleBmp;
-			grayscaleFormatBmp.DestinationFormat = PixelFormats.Gray2;
-			grayscaleFormatBmp.EndInit();
-
-			return grayscaleFormatBmp;
+			var encoder = new JpegBitmapEncoder();
+			encoder.Frames.Add(BitmapFrame.Create(bmp));
+			using (var ms = new MemoryStream())
+			{
+				encoder.Save(ms);
+				var grayscaleMat = Mat.FromStream(ms, ImreadModes.GrayScale);
+				var grayscaleImgStream = grayscaleMat.ToMemoryStream();
+				var result = new BitmapImage();
+				result.BeginInit();
+				result.StreamSource = grayscaleImgStream;
+				result.EndInit();
+				return result;
+			}
 		}
 	}
 }
